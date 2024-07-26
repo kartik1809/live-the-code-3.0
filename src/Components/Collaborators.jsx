17a1dd53645614c834d66504ffbc025948c5bb5d
Kartik1260 <131158199+Kartik1260@users.noreply.github.com>
2024-07-26 17:31:54 +0530

    corrected refs

--- src/Components/Collaborators.jsx
+++ src/Components/Collaborators.jsx
@@ -3,17 +3,17 @@ import './Collaborators.css'
 import Carousel from './Carousel';
 const Collaborators = () => {
     const collaborators = [
-        { src: 'src/assets/anvesh.png', alt: 'Anvesh' },
-        { src: 'src/assets/cc_abes.png', alt: 'CC ABES' },
-        { src: 'src/assets/cc_bv.png', alt: 'CC BV' },
-        { src: 'src/assets/cc_mit.png', alt: 'CC MIT' },
-        { src: 'src/assets/cdao.png', alt: 'CDAO' },
-        { src: 'src/assets/csi.png', alt: 'CSI' },
-        { src: 'src/assets/eve.png', alt: 'EVE' },
-        { src: 'src/assets/gdsc.png', alt: 'GDSC' },
-        { src: 'src/assets/gfg_abes.png', alt: 'GFG ABES' },
-        { src: 'src/assets/gfg.png', alt: 'GFG' },
-        { src: 'src/assets/iee.png', alt: 'IEE' },
+        { src: '/src/assets/anvesh.png', alt: 'Anvesh' },
+        { src: '/src/assets/cc_abes.png', alt: 'CC ABES' },
+        { src: '/src/assets/cc_bv.png', alt: 'CC BV' },
+        { src: '/src/assets/cc_mit.png', alt: 'CC MIT' },
+        { src: '/src/assets/cdao.png', alt: 'CDAO' },
+        { src: '/src/assets/csi.png', alt: 'CSI' },
+        { src: '/src/assets/eve.png', alt: 'EVE' },
+        { src: '/src/assets/gdsc.png', alt: 'GDSC' },
+        { src: '/src/assets/gfg_abes.png', alt: 'GFG ABES' },
+        { src: '/src/assets/gfg.png', alt: 'GFG' },
+        { src: '/src/assets/iee.png', alt: 'IEE' },
     ];
   return (
     <div className='collaborators'>
